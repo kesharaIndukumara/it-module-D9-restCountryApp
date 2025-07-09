d1e90bc494157a59698aadf3122ff607cd773ce1
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-07-09 21:27:21 +0530

    search-function

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,3 +40,38 @@ loadCountries();
 function loadModelData(){
     
 }
+
+function search(){
+  let txtSearchValue = document.getElementById("txtSearch").value;
+
+  let conList = document.getElementById("itemTable")
+
+  let body = "";
+
+  fetch("assets/json/country.json")
+  .then(res => res.json())
+  .then(dataList => {
+    dataList.forEach(element =>{
+      if(element.name.common===txtSearchValue){
+        body = `
+            <div class="col">
+            <div class="card shadow-sm"> 
+            <img src="${element.flags.png}" alt="" class="bd-placeholder-img card-img-top" width="100%" >
+              <div class="card-body">
+              <h5>${element.name.common}</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group"> 
+                    <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick = "loadModelData()">View More </button>
+                  </div> 
+                  <small class="text-body-secondary">9 mins</small>
+                </div>
+              </div>
+            </div>
+          </div>
+            `
+        conList.innerHTML = body;
+      }
+    })
+  })
+}
