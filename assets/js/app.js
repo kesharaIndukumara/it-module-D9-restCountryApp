85dd32b8329ee5ff884200ab74bb27bef674c1b7
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-07-09 19:24:20 +0530

    set-country-api

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+console.log("Hello Kesh")
+
+function loadCountries() {
+    let conList = document.getElementById("itemTable")
+
+    let body = "";
+
+    fetch("assets/json/country.json")
+        .then(res => res.json())
+        .then(dataList => {
+            dataList.forEach(element => {
+                body += `
+            <div class="col">
+            <div class="card shadow-sm"> 
+            <img src="${element.flags.png}" alt="" class="bd-placeholder-img card-img-top" width="100%" >
+              <div class="card-body">
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group"> <button type="button" class="btn btn-sm btn-outline-secondary">View</button> <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> </div> <small class="text-body-secondary">9 mins</small>
+                </div>
+              </div>
+            </div>
+          </div>
+            `
+            });
+
+            conList.innerHTML = body;
+            // console.log(data);
+        })
+}
+
+loadCountries();
