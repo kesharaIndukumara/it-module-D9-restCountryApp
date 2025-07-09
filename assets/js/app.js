3a467db615b4bb430738e9ec885fc1392e6234df
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-07-09 20:22:06 +0530

    set-model

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,8 @@ console.log("Hello Kesh")
 function loadCountries() {
     let conList = document.getElementById("itemTable")
 
+    let modelBody = document.getElementById("model-body")
+
     let body = "";
 
     fetch("assets/json/country.json")
@@ -14,9 +16,13 @@ function loadCountries() {
             <div class="card shadow-sm"> 
             <img src="${element.flags.png}" alt="" class="bd-placeholder-img card-img-top" width="100%" >
               <div class="card-body">
+              <h5>${element.name.common}</h5>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group"> <button type="button" class="btn btn-sm btn-outline-secondary">View</button> <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> </div> <small class="text-body-secondary">9 mins</small>
+                  <div class="btn-group"> 
+                    <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick = "loadModelData()">View More </button>
+                  </div> 
+                  <small class="text-body-secondary">9 mins</small>
                 </div>
               </div>
             </div>
@@ -30,3 +36,7 @@ function loadCountries() {
 }
 
 loadCountries();
+
+function loadModelData(){
+    
+}
